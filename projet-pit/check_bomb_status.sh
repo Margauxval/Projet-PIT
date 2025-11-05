ad82fffa2434846d8a2e2d97b6b6c4ce3446bd10
Margauxval <margauxvallee09@icloud.com>
2025-11-05 17:42:14 +0000

    Delete projet-pit directory

--- projet-pit/check_bomb_status.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# check_bomb_status.sh - affiche le temps restant
-timefile="${1:-time}"
-if [ ! -f "$timefile" ]; then echo "Aucune bombe en cours."; exit 1; fi
-t=$(cat "$timefile" || echo 0)
-echo "Temps restant : ${t}s"
