f63884fcf48a4dcebd9f63ce1a33436e424b578f
Margauxval <margauxvallee09@icloud.com>
2025-11-05 09:44:18 +0000

    Update arcane1_setup.sh

--- Projet-PIT/modules/arcane1_setup.sh
+++ Projet-PIT/modules/arcane1_setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 SANCTUAIRE=./sanctuaire
 GRIMOIRE=.grimoire_secret.txt
-mkdir -p "$SANCTUAIRE"
-if [ ! -f "$SANCTUAIRE/$GRIMOIRE" ]; then
-echo "catulus" > "$SANCTUAIRE/$GRIMOIRE"
-fi
 
-echo "Arcane I prêt. Explore le sanctuaire : $SANCTUAIRE"
+mkdir -p "$SANCTUAIRE"
+[ ! -f "$SANCTUAIRE/$GRIMOIRE" ] && echo "catulus" > "$SANCTUAIRE/$GRIMOIRE"
+
+echo "Arcane I prêt."
+echo "Ton grimoire de Bashicien recueillant toutes tes formules magiques s'est volatilisé ! Il faut que tu le retrouves"
+echo "Explore le sanctuaire : $SANCTUAIRE"
 echo "Quand tu penses avoir trouvé le grimoire, lance : ./modules/verify_arcane1.sh"
