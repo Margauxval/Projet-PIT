fcf5a225cc6444bd5b414d6e6a6b33bd64c16d15
Margauxval <margauxvallee09@icloud.com>
2025-11-05 14:52:39 +0000

    Update arcane1.sh

--- Projet-PIT-ameliore/modules/arcane1.sh
+++ Projet-PIT-ameliore/modules/arcane1.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Arcane 1 — L'Arcane de la Découverte
 set -euo pipefail
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LEVEL="$DIR/arcane1_env"
-VIES_FILE="$DIR/.vies_arcane1"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Définit la variable DIR comme le dossier où se trouve ce script.
+                                                    # "$(dirname "${BASH_SOURCE[0]}")" récupère le chemin du script.
+                                                    # "cd ... && pwd" convertit ce chemin en chemin absolu.
+LEVEL="$DIR/receptacle"                             # Crée une variable LEVEL contenant le chemin vers un sous-dossier "receptacle"
+                                                    # situé au même endroit que le script.
+VIES_FILE="$DIR/.vies_arcane1"                      # Définit le chemin du fichier qui contiendra le nombre de vies du joueur.
 
 rm -rf "$LEVEL"
 mkdir -p "$LEVEL"
@@ -13,11 +16,10 @@ echo "catulus" > "$LEVEL/.grimoire_secret.txt"
 
 echo
 echo "=== Arcane 1 — L'Arcane de la Découverte ==="
-echo "Mission : Découvrir le mot magique caché dans le grimoire secret de ton sanctuaire."
+echo "Ton grimoire de bashicien contenant toutes tes formules magiques vient de se volatiliser !"
+echo "Ta mission : Découvrir le mot magique caché dans le grimoire secret de ton sanctuaire."
 echo
 echo "Tu disposes de 3 vies. Une erreur t'en fera perdre une."
-echo "Tu peux utiliser librement le terminal pour accomplir ta quête."
 echo "Quand tu penses avoir réussi, invoque :"
 echo "  bash modules/verifier_arcane1.sh"
-echo
-echo "Ton sanctuaire de travail : $LEVEL"
+
