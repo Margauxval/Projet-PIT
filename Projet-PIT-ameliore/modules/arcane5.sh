7aa7374c9731322a7f3375c5d7f7778f71bfb1d4
Margauxval <margauxvallee09@icloud.com>
2025-11-05 15:26:15 +0000

    Update arcane5.sh

--- Projet-PIT-ameliore/modules/arcane5.sh
+++ Projet-PIT-ameliore/modules/arcane5.sh
@@ -2,7 +2,7 @@
 # Arcane 5 — L'Archive Finale
 set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LEVEL="$DIR/arcane5_env"
+LEVEL="$DIR/archive_magique"
 VIES_FILE="$DIR/.vies_arcane5"
 
 rm -rf "$LEVEL"
@@ -14,12 +14,11 @@ echo "souvenir" > "$LEVEL/memoire.txt"
 
 echo
 echo "=== Arcane 5 — L'Archive Finale ==="
+echo "Tu as parcouru tellement d'épreuves ce n'est pas le moment de tout oublier !"
 echo "Mission : Créer une archive 'finale.tar.gz' contenant tous les artefacts de ton sanctuaire."
 echo
 echo "Tu disposes de 3 vies. Une erreur t'en fera perdre une."
-echo "Tu peux utiliser librement le terminal pour accomplir ta quête."
 echo "Quand tu penses avoir réussi, invoque :"
 echo "  bash modules/verifier_arcane5.sh"
-echo
-echo "Ton sanctuaire de travail : $LEVEL"
+
 
