bc743ffd8117aeaa390913c23b0b171f727b9eac
Margauxval <margauxvallee09@icloud.com>
2025-11-05 17:42:05 +0000

    Delete Projet-PIT directory

--- Projet-PIT/modules/arcane4_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SANCTUAIRE=./corruption
-mkdir -p "$SANCTUAIRE"
-for f in corrompu1.txt corrompu2.txt normal.txt; do
-  [ ! -f "$SANCTUAIRE/$f" ] && echo "$f" > "$SANCTUAIRE/$f"
-done
-
-echo "Arcane IV prêt. Purifie le sanctuaire en supprimant les fichiers corrompus dans $SANCTUAIRE"
-echo "Quand tu penses avoir nettoyé les fichiers corrompus, lance : ./modules/verify_arcane4.sh"
--- Projet-PIT/modules/verify_arcane4.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-SANCTUAIRE=./corruption
-for f in $SANCTUAIRE/corrompu*.txt; do
-  [ -f "$f" ] && echo "❌ Échec : le fichier corrompu $f existe encore." && exit 1
-done
- echo "✔ Tous les fichiers corrompus ont été supprimés, le Ver disparaît !"
- exit 0
