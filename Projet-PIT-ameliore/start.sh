804015c1a0e73093d2eadef607163f59868bbd5b
Margauxval <margauxvallee09@icloud.com>
2025-11-05 14:42:23 +0000

    Update start.sh

--- Projet-PIT-ameliore/start.sh
+++ Projet-PIT-ameliore/start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+# Active trois options importantes pour rendre le script plus sûr :
+# -e : quitte le script dès qu’une commande renvoie une erreur.
+# -u : provoque une erreur si une variable non initialisée est utilisée.
+# -o pipefail : fait échouer tout pipeline si une commande échoue, pas seulement la dernière.
+
 echo "=== Projet-PIT : L'Apprenti Bashicien ==="
 echo "Prépare-toi, jeune mage... L'Arcane I t'attend."
+
 bash modules/arcane1.sh
+# Exécute le script situé dans le dossier "modules", nommé "arcane1.sh".
+
