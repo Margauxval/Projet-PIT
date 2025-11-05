83f775107c05110f04b55c50ebbfbb05a0a8f1f9
Margauxval <margauxvallee09@icloud.com>
2025-11-05 17:33:47 +0000

    Update arcane5.sh

--- Projet-PIT-ameliore/modules/arcane5.sh
+++ Projet-PIT-ameliore/modules/arcane5.sh
@@ -5,20 +5,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LEVEL="$DIR/archive_magique"
 VIES_FILE="$DIR/.vies_arcane5"
 
-cleanup() {
-    echo "Nettoyage des artefacts de l'Arcane 5..."
-    # Supprime l’ancienne archive si elle existe
-    rm -f "$DIR/finale.tar.gz"
-    # Supprime les dossiers/fichiers temporaires utilisés pour l’archive
-    rm -rf "$DIR/receptacle"
-    rm -f "$DIR/runes.txt"
-
-    # Recréation des fichiers nécessaires pour l’archive finale
-    mkdir -p "$DIR/receptacle"
-    touch "$DIR/receptacle/feu.txt" "$DIR/receptacle/eau.txt" "$DIR/receptacle/air.txt" "$DIR/receptacle/terre.txt"
-    touch "$DIR/runes.txt"
-}
-
 rm -rf "$LEVEL"
 mkdir -p "$LEVEL"
 echo 3 > "$VIES_FILE"
