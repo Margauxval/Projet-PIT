5843741c851da26cb95b4e7d20b3cc480729ca38
Margauxval <margauxvallee09@icloud.com>
2025-11-05 10:37:42 +0000

    Delete Projet-PIT-ameliore directory

--- Projet-PIT-ameliore/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-chmod +x "$SCRIPT_DIR"/modules/*.sh
-echo "=== Projet-PIT : Apprenti Bashicien ==="
-echo "Le jeu commence... Prépare-toi, jeune mage !"
-bash "$SCRIPT_DIR"/modules/arcane1.sh
