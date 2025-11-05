873ca3f38d9f25727617786a300b3061d1ac84ea
Margauxval <margauxvallee09@icloud.com>
2025-11-05 16:27:32 +0000

    Update verifier_arcane2.sh

--- Projet-PIT-ameliore/modules/verifier_arcane2.sh
+++ Projet-PIT-ameliore/modules/verifier_arcane2.sh
@@ -12,19 +12,19 @@ for e in feu eau air terre; do if ! find "$DIR" -type f -path "*/receptacle/$e.t
 
 if [ $success -eq 1 ]; then
   echo
-  echo "✨ Les forces mystiques t'accordent leur faveur !"
-  echo "✔ Tu as triomphé de l'Arcane 2 — Les Éléments Dispersés."
-  echo "→ Le voile se lève... L'Arcane 3 t'appelle."
+  echo "Les forces mystiques t'accordent leur faveur !"
+  echo "Tu as triomphé de l'Arcane 2 — Les Éléments Dispersés."
+  echo "Le voile se lève... L'Arcane 3 t'appelle."
   bash "$DIR/arcane3.sh"
   exit 0
 else
   VIES=$((VIES - 1))
   echo "$VIES" > "$VIES_FILE"
-  echo "❌ L'incantation échoue... Vies restantes : $VIES"
+  echo "L'incantation échoue... Vies restantes : $VIES"
   if [ $VIES -le 0 ]; then
-    echo "💀 Tes pouvoirs t'abandonnent. Tu es renvoyé à l'Arcane 1."
+    echo "Tes pouvoirs t'abandonnent. Tu es renvoyé à l'Arcane 1."
     bash "$DIR/arcane1.sh"
   else
-    echo "↻ Tente à nouveau, mage persévérant. Corrige ton œuvre et relance : bash modules/verifier_arcane2.sh"
+    echo "Tente à nouveau, mage persévérant. Corrige ton œuvre et relance : bash modules/verifier_arcane2.sh"
   fi
 fi
